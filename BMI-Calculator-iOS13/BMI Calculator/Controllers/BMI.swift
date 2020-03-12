2d7f1d10ed01681052ed222b9ef56b98668c3922
Preston Hudson <prestonleehudson@gmail.com>
2020-03-12 07:46:55 -0500

    Updated BMI Calculator to iOS13

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Preston Hudson on 2/4/20.
+//  Copyright © 2020 Spade Software. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
